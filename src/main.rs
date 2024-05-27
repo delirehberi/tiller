f2a3ae8d37cf60aa15dafa4ba11868e326c2cc1a
Emre YILMAZ <z@emre.xyz>
2024-05-27 18:06:26 -0400

    code improvements
    Signed-off-by: Emre YILMAZ <z@emre.xyz>

--- src/main.rs
+++ src/main.rs
@@ -16,25 +16,16 @@ struct Config{
 const APP_NAME: &str = "tiller";
 
 fn main() -> io::Result<()> {
-    let config_path = dirs::home_dir()
-        .expect("failed to find home dir")
-        .join(format!(".config/{}/config.json",APP_NAME))
-        ;
-    let config_data = read_to_string(config_path)?;
-    let config:Config = from_str(&config_data).expect("Invalid config format");
+    let config = config()?;
 
     let Config { editor, til_folder, repo_path } = config;
     let full_til_path = format!("{}/{}",repo_path,til_folder);
 
-    let temp_file = NamedTempFile::new()?;
-    let temp_path = temp_file.path().to_str().unwrap().to_string();
-
-    Command::new(editor)
-        .arg(&temp_path)
-        .status()
-        .expect("Failed to open editor");
-
-    let input_content = read_to_string(temp_path)?;
+    let input_content = get_content(editor)?;
+    if input_content.len() < 1 {
+        println!("You can't save an empty til!");
+        std::process::exit(1)
+    }
 
     let new_file_name = get_next_file_name(&full_til_path)?;
 
@@ -50,26 +41,7 @@ fn main() -> io::Result<()> {
     let mut new_file = File::create(&new_file_path)?;
     write!(new_file,"{} \n {}", prepend_content, input_content)?;
 
-    Command::new("git")
-        .args(&["add", &new_file_path])
-        .current_dir(&repo_path)
-        .status()
-        .expect("Failed to add git");
-
-    Command::new("git")
-        .args(&["commit","-m", &format!("{} created",new_file_name)])
-        .current_dir(&repo_path)
-        .status()
-        .expect("Failed to commit");
-
-    Command::new("git")
-        .args(&["push","origin","master"])
-        .current_dir(&repo_path)
-        .status()
-        .expect("Failed to push");
-
-    Ok(())
-
+    publish(&new_file_path,&repo_path,&new_file_name)
 }
 
 fn get_next_file_name(folder: &str) -> io::Result<String> {
@@ -91,4 +63,48 @@ fn update_prepend_content(template: &str, filename: &str) -> io::Result<String> 
         .replace("$TITLE",&filename_wo_ext)
         .replace("$DATE",&date);
     Ok(content)
+}
+
+fn publish(new_file_path: &str,repo_path: &str,new_file_name: &str) -> io::Result<()>{
+    Command::new("git")
+        .args(&["add", &new_file_path])
+        .current_dir(&repo_path)
+        .status()
+        .expect("Failed to add git");
+
+    Command::new("git")
+        .args(&["commit","-m", &format!("{} created",new_file_name)])
+        .current_dir(&repo_path)
+        .status()
+        .expect("Failed to commit");
+
+    Command::new("git")
+        .args(&["push","origin","master"])
+        .current_dir(&repo_path)
+        .status()
+        .expect("Failed to push");
+    Ok(())
+}
+fn config()->io::Result<Config>{
+
+    let config_path = dirs::home_dir()
+        .expect("failed to find home dir")
+        .join(format!(".config/{}/config.json",APP_NAME))
+        ;
+    let config_data = read_to_string(config_path)?;
+    let config:Config = from_str(&config_data).expect("Invalid config format");
+
+    Ok(config)
+}
+
+fn get_content(editor: String) -> io::Result<String>{
+    let temp_file = NamedTempFile::new()?;
+    let temp_path = temp_file.path().to_str().unwrap().to_string();
+
+    Command::new(editor)
+        .arg(&temp_path)
+        .status()
+        .expect("Failed to open editor");
+
+    read_to_string(temp_path)
 }
